e9f891b4fe2d8d7b6d15730200fc38934e610b2e
João Sarmento <joaovicentesarm@gmail.com>
2022-06-10 09:33:54 -0300

    ajustes necessarios

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getRanking, getUserUrls } from "../controllers/usersController.js";
+import { validateToken } from "../middlewares/urlsMiddleware.js";
 
 
 const usersRouter = Router();
 
-usersRouter.get("/users/ranking", getRanking);
-usersRouter.get("/users/:id", getUserUrls);
+usersRouter.get("/users/:id", validateToken, getUserUrls);
+usersRouter.get("/ranking", getRanking);
 
 export default usersRouter;
